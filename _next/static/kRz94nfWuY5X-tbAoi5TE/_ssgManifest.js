2c923a627898b3d2439bffd5ab4c458a1f8fe6e3
ttnhu <tranthanhnhu5499@gmail.com>
2026-08-20 18:55:31 +0700

    push f

---
+++ _next/static/kRz94nfWuY5X-tbAoi5TE/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fdu-an\u002F[slug]","\u002Fgiai-phap\u002F[slug]","\u002Fsan-pham\u002F[category]","\u002Fsan-pham\u002F[category]\u002F[slug]","\u002Ftin-tuc\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
